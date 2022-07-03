a3ef301ec8cd097e8874d5d1f2e425ddc5a81e85
philocsera <102314260+philocsera@users.noreply.github.com>
2022-07-03 14:31:12 +0900

    [Silver IV] Title: 상근이의 여행, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int T, N, M, a, b;
+	cin >> T;
+	while(T--){
+		cin >> N >> M;
+		while(M--) cin >> a >> b;
+		cout << N-1 << "\n";		
+	}
+}
