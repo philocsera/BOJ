aade247497172861b7845d2bdf206d7a754296a9
philocsera <102314260+philocsera@users.noreply.github.com>
2022-07-20 21:31:00 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9712 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -4,34 +4,29 @@ using namespace std;
 
 int main(){
 	int dp[1000001];
-	vector<int> seq[1000001];
+	int from[1000001];
 	int N;
 	cin >> N;
 
 	for(int i=1;i<=N;i++) dp[i] = 1e9;
 
 	dp[1] = 0;
-	seq[1].push_back(1);
 
 	for(int i=1;i<N;i++){
 		if( 3*i<=N && dp[i]+1 < dp[3*i] ){
 			dp[3*i] = dp[i]+1;
-			seq[3*i] = seq[i];
-			seq[3*i].push_back(3*i);
+			from[3*i] = i;
 		}
 		if( 2*i<=N && dp[i]+1 < dp[2*i] ){
 			dp[2*i] = dp[i]+1;
-			seq[2*i] = seq[i];
-			seq[2*i].push_back(2*i);
+			from[2*i] = i;
 		}
 		if( dp[i]+1 < dp[i+1] ){
 			dp[i+1] = dp[i]+1;
-			seq[i+1] = seq[i];
-			seq[i+1].push_back(i+1);
+			from[i+1] = i;
 		}
 	}
-	int len = seq[N].size();
-	cout << dp[N] << "\n";
-	for(int i=len-1;i>=0;i--) cout << seq[N][i] << " ";
+	cout << dp[N] << "\n" << N << " ";
+	while(N!=1) cout << ( N = from[N] ) << " ";
 
 }
