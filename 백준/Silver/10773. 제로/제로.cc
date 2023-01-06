285f7f43a17b94566cb398db508f7b47f978aa48
philocsera <102314260+philocsera@users.noreply.github.com>
2023-01-06 17:13:13 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,13 +1,2 @@
 #include <iostream>
-int main(){
-	int K,t,N=0,r=0;
-	int s[100'000]={0};
-	std::cin >> K;
-	while(K--){
-		std::cin >> t;
-		if(t) s[N++] = t;
-		else N--;
-	}
-	while(N--) r += s[N];
-	std::cout << r;
-}
+main(){int K,t,N=0,r=0,s[100001]={0};std::cin>>K;while(K--){std::cin>>t;if(t)s[N++]=t;else N--;}while(N--)r+=s[N];std::cout<<r;}
